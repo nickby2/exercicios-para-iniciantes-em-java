d1a95916c13af38ba2ff81848c3aadd81fe3b94b
Lucas Rodrigues Monteiro <monteiro9961@gmail.com>
2022-03-24 16:34:04 -0300

    soma de inteiros

---
+++ src/application/Program1.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		/*
+		 Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses 
+		 números com uma mensagem explicativa, conforme exemplos.
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		int x, y, soma;
+		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		soma = x + y;
+		
+		System.out.println("Soma: " + soma);
+		
+		sc.close();
+
+	}
+
+}
